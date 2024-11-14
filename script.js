157a376ce3f2bc96a64132cbf5fba6ca28f446c7
Zack M. <22827148+lunatichigh@users.noreply.github.com>
2024-11-14 10:57:31 -0500

    Add calculation to evenly space newSquare when new grid is created.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //Use javascript to make a square and create it within #container
+let gridSize = 16;
 
-function createSquares(numSquares) {
+function createSquares() {
     const container = document.getElementById('container'); // Assuming you have a container element
   
-    for (let i = 1; i < 257; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
       // Create a new element (e.g., a div)
-      const newSquare = document.createElement('div');
-      newSquare.className = 'square';
+      const newSquare = document.createElement('newSquare');
+      newSquare.classList.add('square');;
+      newSquare.style.width = (960/gridSize) + 'px';
+      newSquare.style.height = (960/gridSize) + 'px';
         
       // Append the element to the container
       container.appendChild(newSquare);
@@ -14,6 +17,27 @@ function createSquares(numSquares) {
   }
   
 
-  createSquares(16);
+  createSquares();
+
+const button = document.getElementById("resetButton");
 
 
+button.addEventListener("click", function(){
+  const gridSize = parseInt(prompt("Please choose a grid size(No larger than 100):"));
+
+  if (gridSize <= 100 && gridSize > 0){
+    while (container.firstChild)
+      container.firstChild.remove();
+  
+  for (let i = 0; i < (gridSize * gridSize); i++) {
+      // Create a new element (e.g., a div)
+      const newSquare = document.createElement('div');
+      newSquare.className = 'square';
+      newSquare.style.width = (960/gridSize) + 'px';
+      newSquare.style.height = (960/gridSize) + 'px';
+        
+      // Append the element to the container
+      container.appendChild(newSquare);
+    }
+  }
+});
